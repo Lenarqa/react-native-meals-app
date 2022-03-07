3995a7f36e06361d80f82b251fee26310158859a
Lenarqa <37902865+Lenarqa@users.noreply.github.com>
2022-03-08 04:03:19 +0700

    add screens template, add navigator, create firsts navigation

---
+++ screens/FiltersScreen.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const FiltersScreen = (props) => {
+  return (
+    <View>
+      <Text>Filter Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: 'center',
+    }
+});
+
+export default FiltersScreen;
